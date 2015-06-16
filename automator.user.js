b8e3b010025126704fdeb723505b9c8219bf6014
vanZeben <ryan@vanZeben.ca>
2015-06-16 04:24:10 -0400

    Include integrated leaderboard, Fixes ensingm2/SteamMonsterGameScript#127

--- automator.user.js
+++ automator.user.js
@@ -1436,6 +1436,8 @@ var startAll = setInterval(function() {
 		abilities_extra_styles.textContent = '#activeinlanecontainer:hover {height:auto;background:rgba(50,50,50,0.9);padding-bottom:10px;position:absolute;z-index:1} #activeinlanecontainer:hover + #activitylog {margin-top:97px} #activitylog {margin-top: 29px}';
 		document.getElementsByTagName('head')[0].appendChild(abilities_extra_styles);
 
+		//Start leaderboard
+		initLeaderboard();
 	}, 1000);
 
 var endDate = initEndDate();
@@ -1775,3 +1777,133 @@ function toggleSpammer() {
 	}	
 	updateToggle("particles", (spammer == null));
 }
+
+//Leaderboard stuff - pulled from https://github.com/hansskogvold/steamSummerMinigame/commit/f0f905188585e367f42b756a95d459205190b14f
+function initLeaderboard() {
+    var container = document.createElement('div');
+    container.id = 'leaderboard_wrapper';
+    container.style.overflow = "hidden";
+    container.style.height = "360px";
+    container.style.width = "261px";
+    container.style.display = "none";
+    container.style.position = "relative";
+    container.style.margin =  "50px 0 0 5px";
+    container.style.padding = "5px";
+
+    document.getElementById('col_right').appendChild(container);
+
+    var leaderboard = document.createElement('table');
+    leaderboard.id = 'leaderboard';
+
+    var th = document.createElement('tr');
+    th.style.fontSize = '11px';
+    th.style.color = '#ddd';
+
+    var thc = document.createElement('th');
+    var thn = document.createElement('th');
+    var thl = document.createElement('th');
+    thc.appendChild(document.createTextNode('Rank'));
+    thn.appendChild(document.createTextNode('Name'));
+    thl.appendChild(document.createTextNode('Level'));
+
+    th.appendChild(thc);
+    th.appendChild(thn);
+    th.appendChild(thl);
+
+    leaderboard.appendChild(th);
+
+    document.getElementById('leaderboard_wrapper').appendChild(leaderboard);
+
+    var credit = document.createElement('div');
+    credit.style.fontSize = "12px";
+    credit.style.textAlign = "center";
+    credit.innerHTML = 'Data by <a href="http://steamga.me/" style="color:#ddd;" alt="http://steamga.me/" target="_blank">steamga.me</a>';
+
+    document.getElementById('leaderboard_wrapper').appendChild(credit);
+
+    var toggler = document.createElement('div');
+    toggler.id = "leaderboard_toggler";
+    toggler.onclick = function(){
+        toggleLeaderboard();
+    };
+    toggler.style.position = 'absolute';
+    toggler.style.bottom = "-48px";
+    toggler.style.color = "black";
+    toggler.style.textAlign = "center";
+    toggler.style.width = '261px';
+    toggler.style.cursor = "pointer";
+    toggler.appendChild(document.createTextNode("Show Leaderboards"));
+
+    document.getElementById('col_right').appendChild(toggler);
+
+    getLeaderboard();
+    
+    setInterval(function(){ getLeaderboard(); }, 1000 * 30);
+}
+
+function drawLeaderboardRoom(room) {
+    var item = document.createElement('tr');
+    item.className = 'leaderboard_item';
+    item.style.height = '23px';
+    item.style.fontSize = '10px';
+
+    var num = document.createElement('td');
+    num.appendChild(document.createTextNode('#' + room.position));
+
+    var name = document.createElement('td');
+    name.style.textAlign = 'center';
+    name.appendChild(document.createTextNode(room.name));
+
+    var level = document.createElement('td');
+    level.style.textAlign = 'right';
+    level.appendChild(document.createTextNode(room.level));
+    
+    console.log(room.id+" - "+g_GameID);
+    if(room.id == g_GameID) {
+        item.style.color = '#d4e157';
+    }
+
+    item.appendChild(num);
+    item.appendChild(name);
+    item.appendChild(level);
+
+    document.getElementById('leaderboard').appendChild(item);
+}
+
+function getLeaderboard() {
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "http://steamga.me/data/api/leaderboard.json",
+        onload: function(response) {
+            console.log('Downloading new leaderboard...');
+            var elements = document.getElementsByClassName('leaderboard_item');
+            while(elements.length > 0){
+                elements[0].parentNode.removeChild(elements[0]);
+            }
+            var resp = JSON.parse(response.responseText);
+            var leaderboard = Object.keys(resp).map(function (key) {return resp[key]});
+            leaderboard.sort(function(a, b) { 
+                return b.level - a.level;
+            });
+            for each (var room in leaderboard){
+                drawLeaderboardRoom(room);
+            }
+        }
+    });
+}
+
+function toggleLeaderboard() {
+    var a = document.getElementById('leaderboard_wrapper'); 
+    var b = document.getElementById('activitylog'); 
+    var c = document.getElementById('leaderboard_toggler'); 
+    if (a.style.display == 'block') {
+        a.style.display = 'none'; 
+        b.style.display = 'block'; 
+        c.innerHTML = "Show Leaderboards";
+    } else {
+        a.style.display = 'block'; 
+        b.style.display = 'none'; 
+        c.innerHTML = "Show Activity";
+    }
+}
+
