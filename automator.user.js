3610edfd6a0f7a46ac79323a16290950eae3ada1
Michael Ensing <ensingm2@gmail.com>
2015-06-17 22:26:37 -0400

    Version & Minify

--- automator.user.js
+++ automator.user.js
@@ -2,7 +2,7 @@
 // @name [ensingm2] Steam Monster Game Script
 // @namespace https://github.com/ensingm2/SteamMonsterGameScript
 // @description A Javascript automator for the 2015 Summer Steam Monster Minigame
-// @version 2.14
+// @version 2.15
 // @match http://steamcommunity.com/minigame/towerattack*
 // @match http://steamcommunity.com//minigame/towerattack*
 // @updateURL https://raw.githubusercontent.com/ensingm2/SteamMonsterGameScript/master/automator.user.js
